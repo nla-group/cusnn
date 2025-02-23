83cb99fe2e6bad6c33000184ce57deafb2169c33
chenxinye <cclcquant@yahoo.com>
2025-02-23 18:25:14 +0100

    add cmake and example.cpp

---
+++ build/CMakeFiles/snn.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xinye/snn/gpu/snn.cu" "CMakeFiles/snn.dir/snn.cu.o" "gcc" "CMakeFiles/snn.dir/snn.cu.o.d"
+  "/home/xinye/snn/gpu/example.cpp" "CMakeFiles/snn.dir/example.cpp.o" "gcc" "CMakeFiles/snn.dir/example.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
